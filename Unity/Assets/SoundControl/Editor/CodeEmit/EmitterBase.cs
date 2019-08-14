09e603936568e3cd728be195ccd46044f0470dfc
yazawa-ichio <yazawa922473@gmail.com>
2019-08-14 23:22:26 +0900

    CodeEimitを追加

---
+++ Unity/Assets/SoundControl/Editor/CodeEmit/EmitterBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+//namespace ILib.CodeEmit
+namespace ILib.Audio.CodeEmit
+{
+
+	public abstract class EmitterBase
+	{
+		public abstract void Emit(CodeWriter writer);
+	}
+
+}
