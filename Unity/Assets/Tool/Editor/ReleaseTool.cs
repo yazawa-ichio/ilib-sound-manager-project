76428e77360bf57ff79ae83f5b6123d9037be1f2
yazawa-ichio <yazawa922473@gmail.com>
2019-08-20 01:29:54 +0900

    インポートメニューとリリースツールを追加

---
+++ Unity/Assets/Tool/Editor/ReleaseTool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class ReleaseTool
+{
+	[MenuItem("ILib/Audio/Release SoundManager")]
+	static void ReleaseSoundManager()
+	{
+		var name = "Assets/SoundControl/Extension/ILib.SoundManager.unitypackage";
+		var paths = AssetDatabase.GetAllAssetPaths().Where(x => x.StartsWith("Assets/SoundManager"));
+		AssetDatabase.ExportPackage(paths.ToArray(), name, ExportPackageOptions.Default);
+	}
+}
